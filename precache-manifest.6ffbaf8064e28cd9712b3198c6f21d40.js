f719369a24536dae696df54d4d82f14fa2363309
NikitaT1 <nik.tati1@gmail.com>
2020-03-09 16:35:22 +0300

    Updates

---
+++ precache-manifest.6ffbaf8064e28cd9712b3198c6f21d40.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66717969cb26a1aa00545fe8280ee597",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "84106ac34296b394aad4",
+    "url": "/portfolio/static/css/main.c718f157.chunk.css"
+  },
+  {
+    "revision": "78749f9b824c2d782b27",
+    "url": "/portfolio/static/js/2.442378c5.chunk.js"
+  },
+  {
+    "revision": "84106ac34296b394aad4",
+    "url": "/portfolio/static/js/main.584c68c9.chunk.js"
+  },
+  {
+    "revision": "4234e20402381065f478",
+    "url": "/portfolio/static/js/runtime-main.01d0503f.js"
+  },
+  {
+    "revision": "c2723d4932217e3b37b67d38f03bbcb3",
+    "url": "/portfolio/static/media/english.c2723d49.svg"
+  },
+  {
+    "revision": "398d415efd48f051472cbb38be26b8bd",
+    "url": "/portfolio/static/media/iconJS.398d415e.png"
+  },
+  {
+    "revision": "580ffd26ccaa16481ed87c1829c74943",
+    "url": "/portfolio/static/media/iconReact.580ffd26.png"
+  },
+  {
+    "revision": "581af562c35a436d9fc57aafb8192f27",
+    "url": "/portfolio/static/media/phone.581af562.svg"
+  },
+  {
+    "revision": "2d012752738fa7e31c03bbfb386bbdc7",
+    "url": "/portfolio/static/media/photo_2020-03-06_12-56-08.2d012752.jpg"
+  },
+  {
+    "revision": "3203df3aee62ee31fcaff3eb9ef042c0",
+    "url": "/portfolio/static/media/telegram.3203df3a.png"
+  },
+  {
+    "revision": "7f22d71d06b61aa3c820a2bb08e88a30",
+    "url": "/portfolio/static/media/telegram.7f22d71d.svg"
+  },
+  {
+    "revision": "ff20dd3ed47fa692be11f4f6061a10ce",
+    "url": "/portfolio/static/media/typescript.ff20dd3e.png"
+  },
+  {
+    "revision": "2f4f7ea40e4491798ab936d55e12a096",
+    "url": "/portfolio/static/media/viber.2f4f7ea4.svg"
+  }
+]);
